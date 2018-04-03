9d17bc1257172ca4bf945b090a81f04b2a9b9668
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-04-03 17:27:18 -0300

    primeiro commit

---
+++ TesteMapa/TesteMapa/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace TesteMapa
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+            InitializeComponent();
+
+            var mapa = new Map(MapSpan.FromCenterAndRadius(new Position(-29.6905709, -51.1040204), Distance.FromKilometers(2)));
+            mapa.MapType = MapType.Satellite;
+            MapContainer.Children.Add(mapa);
+
+        }
+    }
+}
